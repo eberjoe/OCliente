1a67d494303600d461b54e4afd92889b1aebb4f7
adamiv <valmor_adami_jr@whirlpool.com>
2019-10-17 13:37:40 -0300

    Creating the project.

---
+++ OCliente/src/ocliente/Servidor.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package ocliente;
+
+import java.io.DataOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * A Classe Servidor vai utilizar o design pattern chamado de 
+ * Singleton: Apenas um Objeto desta classe vai existir na 
+ * aplicacao.
+ * 
+ * @author prof-valmor
+ */
+public class Servidor extends Thread {
+    /**
+     * Atributo para abrir o socket com a maquina remota.
+     */
+    private int porta;
+    /**
+     * Atributo para abrir o socket com a maquina remota.
+     */
+    private String ipAddress;
+    /**
+     * Socket aberto com a maquina remota.
+     */
+    private Socket maquinaRemota;
+    /**
+     * Este atributo se refere a unica instancia desta classe que
+     * vai existir.
+     */
+    private static Servidor instance;
+    /**
+     * Nome do usuario que vai se conectar na maquina remota. 
+     * A maquina remota espera esta informacao para poder adicionar na lista 
+     * de usuarios.
+     */
+    private String nomeDoUsuario;
+    
+    /**
+     * Construtor eh privado porque vai existir 
+     * apenas um objeto do tipo Servidor no Cliente.
+     * Isso vai ser garantido atraves do uso do metodo 
+     * getInstance().
+     */
+    private Servidor() {
+        
+    }
+    /**
+     * Esta eh a base do design pattern singleton, tratase de um metod 
+     * static, ou seja, eh um metodo da classe. 
+     * 
+     * Toda vez que for preciso acessar metodos do 
+     * Servidor, vai-se utilizar o getInstance.
+     * @return o objeto Servidor.
+     */
+    public static Servidor getInstance() {
+        if(instance == null) {
+            instance = new Servidor();
+        }
+        return instance;
+    }
+    /**
+     * Metodo para conectar na maquina remota.
+     * @param nomeDoUsuario nome do usuario que vai conectar na maquina remota.
+     * @param ip endereco IP da maquina remota.
+     * @param porta porta aonde conectar.
+     * @throws IOException se houver excessao, vai ser tratada por quem chamou o metodo connect.
+     */
+    public void connect(String nomeDoUsuario, String ip, int porta) throws IOException {
+        maquinaRemota = new Socket(ip, porta);
+        this.nomeDoUsuario = nomeDoUsuario;
+        //
+        this.start();
+    }
+    
+    /**
+     * Servidor eh uma classe filha de Thread, vamos utilizar o metodo run() para
+     * periodicamente ler e enviar dados para a maquina remota.
+     */
+    public void run() {
+        try {
+            // Enviando o nome do usuario para a maquina remota.
+            DataOutputStream dout = new DataOutputStream(maquinaRemota.getOutputStream());
+            dout.writeUTF(nomeDoUsuario);
+
+            do {
+
+            } while (true);
+        } catch (IOException e) {
+
+        }
+    }
+
+}//fim da classe.
