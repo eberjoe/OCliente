76b0fc4eae9a56ab3b63c8dd672110ab5ccc606c
adamiv <valmor_adami_jr@whirlpool.com>
2019-10-24 16:46:20 -0300

    Ajustando a classe para enviar o nome do usuario que se conecta antes de enviar a mensagem para algum usuario.

--- OCliente/src/ocliente/Servidor.java
+++ OCliente/src/ocliente/Servidor.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package ocliente;
 
@@ -12,13 +12,13 @@ import java.io.PrintStream;
 import java.net.Socket;
 
 /**
- * A Classe Servidor vai utilizar o design pattern chamado de 
- * Singleton: Apenas um Objeto desta classe vai existir na 
- * aplicacao.
- * 
+ * A Classe Servidor vai utilizar o design pattern chamado de Singleton: Apenas
+ * um Objeto desta classe vai existir na aplicacao.
+ *
  * @author prof-valmor
  */
 public class Servidor extends Thread {
+
     /**
      * Atributo para abrir o socket com a maquina remota.
      */
@@ -32,78 +32,76 @@ public class Servidor extends Thread {
      */
     private Socket maquinaRemota;
     /**
-     * Este atributo se refere a unica instancia desta classe que
-     * vai existir.
+     * Este atributo se refere a unica instancia desta classe que vai existir.
      */
     private static Servidor instance;
     /**
-     * Nome do usuario que vai se conectar na maquina remota. 
-     * A maquina remota espera esta informacao para poder adicionar na lista 
-     * de usuarios.
+     * Nome do usuario que vai se conectar na maquina remota. A maquina remota
+     * espera esta informacao para poder adicionar na lista de usuarios.
      */
     private String nomeDoUsuario;
-    
+
     /**
-     * Construtor eh privado porque vai existir 
-     * apenas um objeto do tipo Servidor no Cliente.
-     * Isso vai ser garantido atraves do uso do metodo 
+     * Construtor eh privado porque vai existir apenas um objeto do tipo
+     * Servidor no Cliente. Isso vai ser garantido atraves do uso do metodo
      * getInstance().
      */
     private Servidor() {
-        
+
     }
+
     /**
-     * Esta eh a base do design pattern singleton, tratase de um metod 
-     * static, ou seja, eh um metodo da classe. 
-     * 
-     * Toda vez que for preciso acessar metodos do 
-     * Servidor, vai-se utilizar o getInstance.
+     * Esta eh a base do design pattern singleton, tratase de um metod static,
+     * ou seja, eh um metodo da classe.
+     *
+     * Toda vez que for preciso acessar metodos do Servidor, vai-se utilizar o
+     * getInstance.
+     *
      * @return o objeto Servidor.
      */
     public static Servidor getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Servidor();
         }
         return instance;
     }
+
     /**
      * Metodo para conectar na maquina remota.
+     *
      * @param nomeDoUsuario nome do usuario que vai conectar na maquina remota.
      * @param ip endereco IP da maquina remota.
      * @param porta porta aonde conectar.
-     * @throws IOException se houver excessao, vai ser tratada por quem chamou o metodo connect.
+     * @throws IOException se houver excessao, vai ser tratada por quem chamou o
+     * metodo connect.
      */
     public void connect(String nomeDoUsuario, String ip, int porta) throws IOException {
         maquinaRemota = new Socket(ip, porta);
         this.nomeDoUsuario = nomeDoUsuario;
+        // Envinado nome do usuario.
+        OutputStream out = maquinaRemota.getOutputStream();
+        PrintStream ps = new PrintStream(out);
+        ps.println(nomeDoUsuario);
         //
         this.start();
     }
-    
+
     /**
-     * Servidor eh uma classe filha de Thread, vamos utilizar o metodo run() para
-     * periodicamente ler dados da maquina remota.
+     * Servidor eh uma classe filha de Thread, vamos utilizar o metodo run()
+     * para periodicamente ler dados da maquina remota.
      */
     public void run() {
-        try {
-            // Enviando o nome do usuario para a maquina remota.
-            PrintStream dout = new PrintStream(maquinaRemota.getOutputStream());
-            dout.println(nomeDoUsuario);
+        do {
 
-            do {
+        } while (true);
 
-            } while (true);
-        } catch (IOException e) {
-
-        }
     }
 
-    void enviarMensagemPara(String usuario, String texto) throws IOException{
+    void enviarMensagemPara(String usuario, String texto) throws IOException {
         OutputStream out = maquinaRemota.getOutputStream();
         PrintStream ps = new PrintStream(out);
-        
         //
-        String message = usuario+"-> "+texto;
+        String message = usuario + "-> " + texto;
         ps.println(message);
     }
 
